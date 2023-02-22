5a3c8fe2b33709f25b02665812b68f89498f0219
Jakub Krzemiński <jakubkrzeminski2002@gmail.com>
2023-02-22 23:55:54 +0100

    First commit

---
+++ src/test/java/pl/jakub/musiclibrary/MusicLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jakub.musiclibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
